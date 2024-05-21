a9af3ceb207f7746ba39da0b8dec1fec609d9497
DavidAH <cph-dh223@cphbusiness.dk>
2024-05-21 14:15:16 +0200

    Note controler mostly done

--- src/main/java/notes/controlers/NoteController.java
+++ src/main/java/notes/controlers/NoteController.java
@@ -3,14 +3,18 @@ package notes.controlers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.javalin.http.Handler;
+import io.javalin.http.HttpStatus;
 import notes.daos.NoteDAO;
+import notes.dtos.NoteDTO;
+import notes.ressources.Note;
 import notes.utils.TokenUtils;
 
 public class NoteController implements IController {
 
     NoteDAO noteDAO;
     ObjectMapper om;
-    public NoteController(NoteDAO noteDAO){
+
+    public NoteController(NoteDAO noteDAO) {
         this.noteDAO = noteDAO;
         om = new ObjectMapper();
     }
@@ -25,31 +29,57 @@ public class NoteController implements IController {
             var token = (header.get("Authorization").split(" "))[1];
             var userDTO = TokenUtils.getUserWithRolesFromToken(token);
             var usersNotes = noteDAO.getAll(userDTO.getEmail());
+            // TODO error handeling
+            ctx.json(om.writeValueAsString(usersNotes));
         };
     }
 
     @Override
     public Handler getById() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+        return ctx -> {
+            var title = "n1"; // TODO get the rigth thing from the back end
+            var note = noteDAO.getById(title);
+            var noteDTO = new NoteDTO(note);
+            ctx.json(om.writeValueAsString(noteDTO));
+        };
     }
 
     @Override
     public Handler create() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'create'");
+        return ctx -> {
+            Note newNote = ctx.bodyAsClass(Note.class);
+            newNote = noteDAO.create(newNote);
+            String json = om.writeValueAsString(newNote);
+            ctx.status(HttpStatus.CREATED).json(json);
+
+        };
     }
 
     @Override
     public Handler delete() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        return ctx -> {
+            String title = ctx.pathParam("title");
+            Note note = noteDAO.getById(title);
+            noteDAO.delete(note);
+            ctx.status(HttpStatus.NO_CONTENT);
+        };
     }
 
     @Override
     public Handler update() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        return ctx -> {
+            String title = ctx.pathParam("title");
+            Note changedNote = ctx.bodyAsClass(Note.class);
+            Note note = noteDAO.getById(title);
+
+            note.setContent(changedNote.getContent());
+            // TODO set up more of the changes
+
+            noteDAO.update(note);
+
+            String json = om.writeValueAsString(note);
+            ctx.status(HttpStatus.OK).json(json);
+        };
     }
-    
+
 }
